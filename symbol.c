d94222d770ec3847bf2075355fb8f585c544ae18
Jada-Sindhu <sindhuns02@gmail.com>
2026-04-28 11:15:52 +0530

    Add files via upload

---
+++ symbol.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    char ch;
+    printf("Enter the character \n");
+    scanf("%c",&ch);
+    int res=!(ch>='A')&&(ch<='Z')||!(ch>='a')&&(ch<='z')||!(ch>='0')&&(ch<='9');
+    printf("%c is symbol i.e is:%d",ch,res);
+}
